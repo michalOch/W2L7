602762dc7e8602fc8819eceed4fcdd1467b15d3f
michalOch <ochrobot1990@gmail.com>
2020-11-08 16:11:21 +0100

    Checking if number is even or odd.

--- LogicOperators/Program.cs
+++ LogicOperators/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace LogicOperators
 {
@@ -17,7 +18,30 @@ namespace LogicOperators
         }
         public static void Ex2()
         {
-
+            int number = 0;
+            while(number == 0)
+            {
+                Console.Write("Podaj liczbę całkowitą: ");
+                string result = Console.ReadLine();
+                if(int.TryParse(result, out number))
+                {
+                    if(number%2 == 0)
+                    {
+                        Console.WriteLine($"Liczba {number} jest parzysta");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Liczba {number} jest nie parzysta");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Podano nieprawidłową liczbę");
+                    Thread.Sleep(500);
+                    Console.Clear();
+                }
+            }
+            
         }
     }
 }
