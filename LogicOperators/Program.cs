73e7e80810459dd072c67341b00cdb38d4f14d32
michalOch <ochrobot1990@gmail.com>
2020-11-08 20:48:15 +0100

    Avaliable position depending on candidate age.

--- LogicOperators/Program.cs
+++ LogicOperators/Program.cs
@@ -116,7 +116,54 @@ namespace LogicOperators
 
         public static void Ex5()
         {
+            /*
+             * Napisz program w C#, który sprawdzi czy podany przez użytkownika wiek uprawnia go do ubiegania się o stanowisko posła, premiera, sentarora, prezydenta.
+             */
 
+            int age = 0;
+            while (age == 0 || age < 0)
+            {
+                Console.Write("Podaj wiek kandytana (liczba całkowita > 0): ");
+                string result = Console.ReadLine();
+                string possiblePossition = string.Empty;
+                if (int.TryParse(result, out age) && age > 0)
+                {
+                    if(age > 0 && age < 18)
+                    {
+                        possiblePossition = "Jesteś za młody na dostępne stanowiska.";
+                    }
+                    else if(age >= 18 && age < 21)
+                    {
+                        possiblePossition = "Możesz zostać radnym.";
+                    }
+                    else if (age >= 21 && age < 30)
+                    {
+                        possiblePossition = "Możesz zostać posłem";
+                        if(age >= 25)
+                        {
+                            possiblePossition += " oraz wójtem, burmistrzem i prezydentem miasta.";
+                        }
+                    }
+   
+                    else if(age >= 30 && age < 35)
+                    {
+                        possiblePossition = "Możesz zostać senatorem.";
+                    }
+                    else if( age >= 35)
+                    {
+                        possiblePossition = "Możesz zostać prezydentem.";
+                    }
+
+                    Console.WriteLine(possiblePossition);
+                }
+
+                else
+                {
+                    Console.WriteLine("Podano nieprawidłową wartość. Spróbuj ponownie...");
+                    Thread.Sleep(500);
+                    Console.Clear();
+                }
+            }
         }
     }
 }
