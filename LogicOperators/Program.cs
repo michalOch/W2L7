aeb2fdcf9663797cdd09460e18a9229025af2727
michalOch <ochrobot1990@gmail.com>
2020-11-08 15:57:28 +0100

    "First Commit"

--- LogicOperators/Program.cs
+++ LogicOperators/Program.cs
@@ -6,14 +6,18 @@ namespace LogicOperators
     {
         public static void Main(string[] args)
         {
-            Ex1();
+            //Ex1();
+            Ex2();
         }
         public static void Ex1()
         {
             int firstNubmer = 5;
             int secondNumber = 5;
-
             Console.WriteLine($"{firstNubmer} == {secondNumber} ? : {firstNubmer == secondNumber}");
+        }
+        public static void Ex2()
+        {
+
         }
     }
 }
