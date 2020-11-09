b6caa648441b774e323fdf0d1a665090f856a768
michalOch <ochrobot1990@gmail.com>
2020-11-09 10:41:46 +0100

    Add enum structure to project

---
+++ LogicOperators/WeekDay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicOperators
+{
+    public enum WeekDay
+    {
+        Unassigned = 0,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
